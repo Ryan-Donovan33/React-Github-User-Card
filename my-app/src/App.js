36f34e34feb4641dbadb88f2ef3e3a0bdcabd74f
Ryan-Donovan33 <55716909+Ryan-Donovan33@users.noreply.github.com>
2020-02-14 20:42:58 -0500

    reading as undefined when fetching data

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 import GitUser from './components/GitUser';
+import axios from 'axios';
 
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			users: []
+			usersData: {},
+			followersData: []
 		};
 	}
 
 	componentDidMount() {
-		fetch('https://api.github.com/users')
-			.then((res) => res.json())
-			.then((users) => this.setState({ users: users.data }))
+		axios('https://api.github.com/users/Ryan-Donovan33')
+			.then((users) => this.setState({ usersData: users.data }))
 			.catch((err) => console.log('No Users!', err));
 		console.log('componentDidMount running');
-	}
 
-	fetchUsers = (e) => {
-		e.preventDefault();
-
-		fetch('https://api.github.com/users/Ryan-Donovan33/followers')
-			.then((res) => res.json())
-			.then((users) => this.setState({ users: users.data.followers }))
+		axios('https://api.github.com/users/Ryan-Donovan33/followers')
+			.then((users) => this.setState({ followersData: users.data }))
 			.catch((err) => console.log('No followers', err));
-	};
+	}
 
 	render() {
 		console.log(this.state.users);
@@ -33,6 +29,10 @@ class App extends Component {
 			<div className="App">
 				<header className="App-header" />
 				<GitUser />
+				<h2> {this.state.usersData.login}</h2>
+				<p>{this.state.usersData.name} </p>
+				<img src={this.state.usersData.avatar} alt="profile pic" />
+				<div />
 			</div>
 		);
 	}
